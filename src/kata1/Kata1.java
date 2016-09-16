c5224f8881fe765c4211f90f52bab4f7abebe4a8
joframontesdeocanuez <joframontesdeocanuez@10.230.175.22>
2016-09-16 09:22:18 +0100

    Primera versión de Kata1 en donde se usa Date (añadiendo campo apellido)

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -5,7 +5,7 @@ import java.util.Date;
 public class Kata1 {
 
     public static void main(String[] args) {
-        Person person = new Person("Juan Francisco", new Date(96,11,21));
+        Person person = new Person("Juan Francisco", "Montesdeoca", new Date(96,11,21));
         System.out.println(person.getName()+ " tiene "+ person.getAge() + " años");
              
     }
--- src/kata1/Person.java
+++ src/kata1/Person.java
@@ -5,10 +5,12 @@ import java.util.Date;
 
 public class Person {
     private final String name;
+    private final String apellido;
     private final Date birthdate;
 
-    public Person(String name, Date birthdate) {
+    public Person(String name, String apellido, Date birthdate) {
         this.name = name;
+        this.apellido = apellido;
         this.birthdate = birthdate;
     }
 
